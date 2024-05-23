50aa0ac5d31722c78dc02a80a34ad3f08e76ac5d
carlo <ste.maragno@gmail.com>
2024-05-23 02:28:46 +0200

    working ish, js resize is broken. I'll ask tati

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 
 from flask import Flask, request, render_template
+from collections import defaultdict
 import sqlite3
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -15,13 +20,17 @@ def symbols():
 
 @app.route('/search', methods=['POST'])
 def search():
-    symbol_name = request.form['symbol_name']
+    symbol_name = request.json['symbol_name']
     conn = sqlite3.connect('symbols.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM symbols WHERE name GLOB ?", (symbol_name,))
+    c.execute("SELECT * FROM symbols WHERE name GLOB ? ORDER BY file", (symbol_name,))
     results = c.fetchall()
+    sym_in_files = defaultdict(list)
+    for r in results:
+        sym_in_files[r[3]].append(r)
     conn.close()
-    return render_template('results.html', results=results, symbol_name=symbol_name)
+    return sym_in_files
+    # return render_template('results.html', results=results, symbol_name=symbol_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/js/magic.js
@@ -0,0 +1,144 @@
+
+const textbox = document.getElementById('wow_many_syms');
+const table = document.getElementById('such_table');
+
+
+let startX, startWidthColumn1, startWidthColumn2;
+
+
+// let resizer = document.querySelector('.resizer');
+// resizer.addEventListener('mousedown', initResize);
+
+textbox.addEventListener('input', handleInput);
+textbox.addEventListener('keydown', handleKeydown);
+
+
+
+function initResize(e) {
+    const column1 = document.querySelector('.column-1');
+    const column2 = document.querySelector('.column-2');
+    startX = e.clientX;
+    startWidthColumn1 = column1.offsetWidth;
+    startWidthColumn2 = column2.offsetWidth;
+
+    window.addEventListener('mousemove', resizeColumns);
+    window.addEventListener('mouseup', stopResize);
+}
+
+function resizeColumns(e) {
+    const column1 = document.querySelector('.column-1');
+    const column2 = document.querySelector('.column-2');
+    const dx = e.clientX - startX;
+    const newWidthColumn1 = startWidthColumn1 + dx;
+    const newWidthColumn2 = startWidthColumn2 - dx;
+
+    column1.style.width = `${newWidthColumn1}px`;
+    column2.style.width = `${newWidthColumn2}px`;
+}
+
+function stopResize() {
+    window.removeEventListener('mousemove', resizeColumns);
+    window.removeEventListener('mouseup', stopResize);
+}
+
+function clearTable() {
+  // Remove all rows except the first one
+  while (table.rows.length >=1) {
+    table.deleteRow(0);
+  }
+}
+
+
+async function fetchData(sym) {
+    try {
+        const href = window.location.href;
+        const response = await fetch(href + "search", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ symbol_name:  sym})
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        clearTable();
+        const data = await response.json();
+        const mainTable = document.querySelector('.container table');
+
+        // Assuming the response is an array of objects
+        Object.entries(data).forEach(([key, value]) => {
+          console.log(`Key: ${key}, Value: ${value}`);
+          // Create a new row
+          const newRow = document.createElement('tr');
+          newRow.classList.add('biig-rows');
+
+          // Column 1 content
+          const column1Content = document.createElement('td');
+          column1Content.classList.add('column', 'column-1');
+          column1Content.textContent = key;
+
+          // Column 2 content
+          const column2Content = document.createElement('td');
+          column2Content.classList.add('column', 'column-2');
+
+          // Create inner table
+          const innerTable = document.createElement('table');
+          value.forEach(sym => {
+            const innerRow = document.createElement('tr');
+            const address = document.createElement('td');
+            address.textContent = sym[0];
+            address.classList.add('sym_address')
+
+            const type = document.createElement('td');
+            type.textContent = sym[1];
+            type.classList.add('sym_type')
+
+            const name = document.createElement('td');
+            name.textContent = sym[2];
+            name.classList.add('sym_name')
+            
+
+            innerRow.appendChild(name);
+            innerRow.appendChild(type);
+            innerRow.appendChild(address);
+
+            innerTable.appendChild(innerRow);
+          });
+          column2Content.appendChild(innerTable);
+
+          // Append columns to the new row
+          newRow.appendChild(column1Content);
+          const resizer = document.createElement('td');
+          resizer.classList.add('resizer')
+          resizer.addEventListener('mousedown', initResize);
+          newRow.appendChild(resizer); // Empty cell for the resizer
+          newRow.appendChild(column2Content);
+
+          // Append the new row to the main table
+          mainTable.appendChild(newRow);
+        });        
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+let lastPrintedTime = 0;
+
+function handleInput(event) {
+    const currentTime = new Date().getTime();
+    if (currentTime - lastPrintedTime >= 100 && event.target.value.length > 6) { // 1000 milliseconds = 1 second
+        fetchData(event.target.value);
+        lastPrintedTime = currentTime;
+    }
+}
+
+function handleKeydown(event) {
+    if (event.key === 'Enter') {
+        fetchData(event.target.value);
+    }
+}
+// Get the textbox element
+
+// Attach the input event listener to the textbox
